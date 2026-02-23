7f26a5a5876a7f266e1610c4cf5357022a608d8e
Joseph Khella <josephkhella2010@gmail.com>
2026-02-23 20:36:51 +0100

    Run migrations

--- backend/api/schema/userInfo_schema.py
+++ backend/api/schema/userInfo_schema.py
@@ -1,14 +1,4 @@
-""" from django.db import models
-class User(models.Model):
-    username=models.CharField()
-    email=models.CharField(unique=True)
-    password=models.CharField()
-    items=models.JSONField(default=list, blank=True)
 
-    def __str__(self):
-            return f"username={self.username}, email={self.email}, password={self.password}, items={self.items}"
-
- """
 from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=150)
