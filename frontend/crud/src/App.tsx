e6b24c48a194d8aaa874beec897a6a1078e54b7c
Joseph Khella <josephkhella2010@gmail.com>
2026-02-24 21:52:20 +0100

    when u close logout

--- frontend/crud/src/App.tsx
+++ frontend/crud/src/App.tsx
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
 import "./App.css";
 import RoutesPage from "./pages/Routes/RoutesPage";
 
 function App() {
+  useEffect(() => {
+    const handleUnload = () => {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+    };
+
+    window.addEventListener("beforeunload", handleUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleUnload);
+    };
+  }, []);
   return (
     <>
       <RoutesPage />
