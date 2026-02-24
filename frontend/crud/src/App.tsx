7d94a38065c1795ff7bbc0edac3d582bb8bf9907
Joseph Khella <josephkhella2010@gmail.com>
2026-02-24 22:02:17 +0100

    remove reload code

--- frontend/crud/src/App.tsx
+++ frontend/crud/src/App.tsx
@@ -1,20 +1,7 @@
-import { useEffect } from "react";
 import "./App.css";
 import RoutesPage from "./pages/Routes/RoutesPage";
 
 function App() {
-  useEffect(() => {
-    const handleUnload = () => {
-      localStorage.removeItem("token");
-      localStorage.removeItem("user");
-    };
-
-    window.addEventListener("beforeunload", handleUnload);
-
-    return () => {
-      window.removeEventListener("beforeunload", handleUnload);
-    };
-  }, []);
   return (
     <>
       <RoutesPage />
