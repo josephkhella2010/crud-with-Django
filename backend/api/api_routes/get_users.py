b166e739c99ee09d081b732c6660ef720c69ef9c
Joseph Khella <josephkhella2010@gmail.com>
2026-03-03 22:45:12 +0100

    fix error

--- backend/api/api_routes/get_users.py
+++ backend/api/api_routes/get_users.py
@@ -1,19 +1,3 @@
-""" from django.shortcuts import render
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from .add_user import users
-
-
-@csrf_exempt
-def get_users(req):
-    try:
-        if req.method=="GET":
-           return  JsonResponse({"users":users})
-
-    except Exception as e:
-        return JsonResponse({"error":str(e)},status=500)
- """
-
 
 from django.shortcuts import render
 from django.http import JsonResponse
@@ -24,7 +8,8 @@ def get_users(req):
     try:
         if req.method=="GET":
             users = User.objects.all()
-            users_list = [u.to_dict() for u in users]  
+            users_list = [u.to_schema() for u in users]
             return JsonResponse({"users": users_list}, status=200)
+
     except Exception as e:
         return JsonResponse({"error":str(e)},status=400)
